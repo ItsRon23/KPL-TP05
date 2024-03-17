b3906147a3b365334cbbe3a2c5699e91a3a85e12
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-03-17 20:32:55 +0700

    HaloGeneric

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more 
 
-Console.WriteLine("Hello World");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric.SapaUser<string>("Aaron Joseph")
+    }
+}
+
+class HaloGeneric
+{
+    public static void SapaUser<T>(T X)
+    {
+        Console.WriteLine($"Halo user {X}", X);
+    }
+}
+
